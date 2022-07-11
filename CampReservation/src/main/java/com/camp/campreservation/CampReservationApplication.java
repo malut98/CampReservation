2a014ba90d206107c2cadbcf5b3d3328bc3cf11e
malut98 <aokimki1@naver.com>
2022-07-11 10:14:02 +0900

    das

--- CampReservation/src/main/java/com/camp/campreservation/CampReservationApplication.java
+++ CampReservation/src/main/java/com/camp/campreservation/CampReservationApplication.java
@@ -2,13 +2,18 @@ package com.camp.campreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CampReservationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampReservationApplication.class, args);
-		System.out.println("d");
 	}
-
+	@RequestMapping("/")
+	public String root() {
+		return "index";
+	}
 }
+
+
