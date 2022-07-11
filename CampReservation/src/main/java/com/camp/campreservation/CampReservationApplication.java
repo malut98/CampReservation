54a3f1b986828ac488e03bdc912e5487094cc238
malut98 <aokimki1@naver.com>
2022-07-11 10:03:30 +0900

    aS

---
+++ CampReservation/src/main/java/com/camp/campreservation/CampReservationApplication.java
@@ -0,0 +1,14 @@
+package com.camp.campreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampReservationApplication.class, args);
+		System.out.println("d");
+	}
+
+}
---
+++ CampReservation/src/test/java/com/camp/campreservation/CampReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.campreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
