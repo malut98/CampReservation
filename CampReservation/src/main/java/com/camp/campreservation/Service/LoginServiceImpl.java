f827afe34f2cbacc1e26859185dac86cb1bb5c43
yyci12 <yyci@naver.com>
2022-07-20 09:55:02 +0900

    sign up ajax

--- CampReservation/src/main/java/com/camp/campreservation/Service/LoginServiceImpl.java
+++ CampReservation/src/main/java/com/camp/campreservation/Service/LoginServiceImpl.java
@@ -2,6 +2,7 @@ package com.camp.campreservation.Service;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
